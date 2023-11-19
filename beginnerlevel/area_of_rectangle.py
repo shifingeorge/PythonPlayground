598a6bb4ba81e04c16062fd77bfabb4ece83bffb
shifiyy <shifingeorgemaliyakkal@gmail.com>
2023-11-19 21:47:14 +0530

    Day 1 code

---
+++ beginnerlevel/area_of_rectangle.py
@@ -0,0 +1,10 @@
+# Write a Python program that calculates the area of a rectangle given its length and width (values provided as variables).
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate the area of the rectangle
+area = length * width
+
+# Print the area of the rectangle
+print("The area of the rectangle is:", area)
